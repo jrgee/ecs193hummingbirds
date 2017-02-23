f937227c9d752441db90d624e3cfca52cdd467a1
giheok <giheok.nam@gmail.com>
2017-02-22 21:48:37 -0800

    Implement Auxillary fields

--- app/src/main/java/beetag/zxingtest/cameraActivity.java
+++ app/src/main/java/beetag/zxingtest/cameraActivity.java
@@ -4,14 +4,31 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.view.View;
 
 //everything in this file is hacked together from various sources on Stack Overflow
 //this is intended as a proof of concept rather than any semblance of final code
 
 
 public class cameraActivity extends AppCompatActivity {
+    public Button list_view, snap_view;
+
+    public void init() {
+
+        list_view = (Button) findViewById(R.id.button2);
+
+        list_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toy = new Intent(cameraActivity.this, tagFields.class);
+                startActivity(toy);
+            }
+        });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
