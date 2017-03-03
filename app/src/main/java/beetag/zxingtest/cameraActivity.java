c5096c2b37398603ae9233bac802b37650101c4e
giheok <giheok.nam@gmail.com>
2017-03-02 16:18:13 -0800

    Fix layout

--- app/src/main/java/beetag/zxingtest/cameraActivity.java
+++ app/src/main/java/beetag/zxingtest/cameraActivity.java
@@ -12,48 +12,14 @@ import android.view.View;
 import android.content.ClipboardManager;
 import android.content.ClipData;
 
+import org.w3c.dom.Text;
+
 /**
- * The cameraActivity class is used to display information passed from the previous camera screen
+ * The cameraActivity class is used to display information passed from the previous activity
  */
 public class cameraActivity extends AppCompatActivity {
-    public Button listView, snapView;
-    CharSequence decimalLabel;
-    Intent intent = getIntent();
-    final String decimalValue = intent.getStringExtra("decimal");
-
-    /**
-     ** Initalizes the activity and sets up a listener for the button press in order to move to the
-     * tag field screen
-     */
-    public void init() {
-
-        // Receive BEEtag data stored in previous activity
-        Intent intent = getIntent();
-        final String decimalValue = intent.getStringExtra("decimal");
-
-        // Opens next page with Auxillary Marker button press
-        listView = (Button) findViewById(R.id.button2);
-        listView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Upon button press, go to Auxillary Markers page
-                Intent toy = new Intent(cameraActivity.this, tagFields.class);
-                startActivity(toy);
-            }
-        });
-
-        // Sets up Copy button process
-        /*snapView = (Button) findViewById(R.id.button3);
-        snapView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Copies the decimal value onto clipboard
-                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                ClipData clip = ClipData.newPlainText(decimalLabel, decimalValue);
-                clipboard.setPrimaryClip(clip);
-            }
-        });*/
-    }
+    public Button listView;
+    public TextView snapView;
 
     /**
      * Sets up actions to be taken upon entering the cameraActivity screen including retrieving the
@@ -66,22 +32,39 @@ public class cameraActivity extends AppCompatActivity {
         setContentView(R.layout.activity_camera);
         TextView decStr = (TextView) findViewById(R.id.decText);
 
-        // Receive BEEtag data stored in previous activity and display data on screen
+        // Receive BEEtag data stored in previous activity
         Intent intent = getIntent();
         final String decimalValue = intent.getStringExtra("decimal");
-        decStr.setText(decimalValue);
+        //decStr.setText(decimalValue);
+
+        // Opens next page with Auxillary Marker button press
+        listView = (Button) findViewById(R.id.button2);
+        listView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Upon button press, go to Auxillary Markers page
+                Intent toy = new Intent(cameraActivity.this, tagFields.class);
+                startActivity(toy);
+            }
+        });
     }
 
-    /**
-     * Clicking the decimal number will copy the data from the clipboard
-     * @param v the current screen view
-     */
-    public void textViewClick(View v){
-        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-        ClipData clip = ClipData.newPlainText(decimalLabel, decimalValue);
-        clipboard.setPrimaryClip(clip);
+        public void textViewClick(View v){
+
+             // Sets up Copy button process
+            snapView = (TextView) findViewById(R.id.decText);
+            final String decimalValue = snapView.getText().toString();
+            snapView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Copies the decimal value onto clipboard
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("label", decimalValue);
+                clipboard.setPrimaryClip(clip);
+            }
+        });
+
     }
 
-    // Do not need to display thumbnail of image taken
 
 }
